b0f6c63c387c5e5945d43cde399c6ffa7109f82e
Pedro <pedrosantosmaciel@gmail.com>
2024-08-04 14:16:01 -0300

    inserção da navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./app.css";
+import "./App.css";
 import logo from "./assets/IconAppClickgas.png";
 
 const Home = () => {
---
+++ src/pages/myDream/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.css';
+
+const MyDream = () => {
+  return (
+    <div className={styles.content}>
+      <h1 className={}>My Dream </h1>
+    </div>
+  );
+}
+
+export default MyDream;

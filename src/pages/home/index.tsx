e22953e27e860dce5989c4437fba64a4ade9b621
Pedro <pedrosantosmaciel@gmail.com>
2024-08-06 17:47:06 -0300

    -up

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,22 +1,42 @@
+import { useState } from "react";
 
 
 const Home = () => {
+const [gas, setGas] = useState<boolean>(false);
+
+
+
+  const gasF = ()=> {
+    console.log('clicou no gas');
+  }
+  const dream = ()=> {
+    console.log('clicou no dream');
+  }
+
   return (
-    <section className="flex  ">
+    <section className="flex  flex-col p-1">
 
 
-      <nav className="w-full border-2 h-20 bg-black rounded-2xl">
+      <nav className="w-full border-2 h-20 bg-black rounded-2xl flex-wrap">
         <div className="w-4/5 border-2 h-12 mx-auto my-4 rounded-2xl">
           <ul className="flex justify-around w-full my-2 ">
-            <li className="text-white">Politica Click Gás e Água</li>
-            <li className="text-white">Politica My Dream</li>
+            <li className="text-white hover:text-[#BC000D] hover:cursor-pointer" onClick={gasF}>Politica Click Gás e Água</li>
+            <li className="text-white hover:text-[#B9307F] hover:cursor-pointer" onClick={dream}>Politica My Dream</li>
           </ul>
         </div>
       </nav>
-      
+      <h1 className="text-center text-4xl">Clique Em cima de algum dos icones dos Apps, ou no menu acima pra visualizar as Politicas de privacidades referentes a cada um </h1>
+
+      <div className="flex">
 
 
-    </section>
+      </div>
+      {
+        
+
+      }
+
+    </section> 
   );
 }
 

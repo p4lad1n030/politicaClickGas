e627a2cac36fdcc0cc01f7145240a246d41e4994
Pedro <pedrosantosmaciel@gmail.com>
2024-08-06 16:26:12 -0300

    -l

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,9 +2,20 @@
 
 const Home = () => {
   return (
-    <section className="flex justify-center ">
+    <section className="flex  ">
+
+
+      <nav className="w-full border-2 h-20 bg-black rounded-2xl">
+        <div className="w-4/5 border-2 h-12 mx-auto my-4 rounded-2xl">
+          <ul className="flex justify-around w-full my-2 ">
+            <li className="text-white">Politica Click Gás e Água</li>
+            <li className="text-white">Politica My Dream</li>
+          </ul>
+        </div>
+      </nav>
       
-      <h1 className="">Home</h1>
+
+
     </section>
   );
 }

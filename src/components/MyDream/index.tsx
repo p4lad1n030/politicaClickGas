6ff5e26aeeeeb7d0813c65ab1d9b6fb00ae07186
Pedro <pedrosantosmaciel@gmail.com>
2024-08-08 16:15:46 -0300

    -f

--- src/components/MyDream/index.tsx
+++ src/components/MyDream/index.tsx
@@ -1,9 +1,37 @@
-import styles from './index.module.css';
+
 
 const MyDream = () => {
   return (
-    <div className={styles.content}>
-      <h1 className={styles.h1}>My Dream </h1>
+    <div className='flex flex-col p-8'>
+      <h1 className='text-center text-5xl font-bold text-[#B9307F]'>Política de Privacidade My Dream </h1>
+      
+      <h2 className="text-3xl">Coleta de Dados</h2>
+      <h3 className="text-2xl mb-4 text-justify">
+      Nosso aplicativo de interpretação de sonhos não coleta informações pessoais dos usuários. A única informação compartilhada é o sonho relatado pelo usuário através do campo de entrada de texto.
+      </h3>
+      <h2 className="text-3xl">Finalidade e Necessidade  </h2>
+      <h3 className="text-2xl mb-4 text-justify">
+        A coleta do sonho do usuário é necessária exclusivamente para a interpretação dos sonhos. Não utilizamos esses dados para nenhum outro propósito.
+      </h3>
+      
+
+      <h2 className="text-3xl">Base Legal  </h2>
+      <h3 className="text-2xl mb-4 text-justify">
+         Não há base legal para o tratamento de registros, uma vez que não coletamos dados pessoais. A interpretação dos sonhos é realizada com base no consentimento implícito do usuário ao compartilhar seu sonho.
+      </h3>
+     
+
+      <h2 className="text-3xl">Armazenamento e Segurança  </h2>
+      <h3 className="text-2xl mb-4 text-justify">
+        Nenhum dado é armazenado em nosso aplicativo. As conversas são perdidas quando o aplicativo é fechado. Garantimos a segurança e privacidade dos dados do usuário por meio dessa abordagem.
+      </h3>
+      
+
+      <h2 className="text-3xl">Compartilhamento com Terceiros  </h2>
+      <h3 className="text-2xl mb-4 text-justify">
+        Não há integração com terceiros em nosso aplicativo, exceto o uso da API de chat GT para interpretação dos sonhos.
+      </h3>
+      
     </div>
   );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
+import clickGas from '../../assets/IconAppClickgas.png';
+import ClickGas from '../../components/ClickGas';
+import myDream from '../../assets/myDream.png';
+import MyDream from "../../components/MyDream";
+
 
 
 const Home = () => {
-const [gas, setGas] = useState<boolean>(false);
+  const [gas, setGas] = useState<boolean>(false);
+  const [dream, setDream] = useState<boolean>(false);
+  const [show, setShow] = useState<boolean>(false);
 
 
 
-  const gasF = ()=> {
-    console.log('clicou no gas');
+  const gasF = () => {
+    setShow(true)
+    setGas(true)
+    setDream(false)
   }
-  const dream = ()=> {
-    console.log('clicou no dream');
+  const dreamF = () => {
+    setShow(true)
+    setDream(true)
+    setGas(false)
+
   }
 
   return (
@@ -21,22 +33,39 @@ const [gas, setGas] = useState<boolean>(false);
         <div className="w-4/5 border-2 h-12 mx-auto my-4 rounded-2xl">
           <ul className="flex justify-around w-full my-2 ">
             <li className="text-white hover:text-[#BC000D] hover:cursor-pointer" onClick={gasF}>Politica Click Gás e Água</li>
-            <li className="text-white hover:text-[#B9307F] hover:cursor-pointer" onClick={dream}>Politica My Dream</li>
+            <li className="text-white hover:text-[#B9307F] hover:cursor-pointer" onClick={dreamF}>Politica My Dream</li>
           </ul>
         </div>
       </nav>
-      <h1 className="text-center text-4xl">Clique Em cima de algum dos icones dos Apps, ou no menu acima pra visualizar as Politicas de privacidades referentes a cada um </h1>
+      <h1 className="text-center text-4xl">Clique em cima de algum dos icones dos Apps, ou no menu acima pra visualizar as Politicas de privacidade referentes a cada um  </h1>
 
-      <div className="flex">
+      <div className="flex justify-around items-center bg-slate-400 w-9/12 mx-auto p-8">
 
+        <div className="bg-white p-12 rounded-2xl hover:cursor-pointer" onClick={gasF}>
+          <img src={clickGas} alt="icone click gas" className='w-[175px] rounded-xl' />
+          <h2 className="text-center font-semibold">Politica Click Gás e Água</h2>
+        </div>
+
+        <div className=" bg-white p-12 rounded-2xl hover:cursor-pointer" onClick={dreamF}>
+          <img src={myDream} alt="icone my dream" className='w-[175px] rounded-xl' />
+          <h2 className="text-center font-semibold">Politica My Dream</h2>
+        </div>
 
       </div>
       {
-        
-
+        gas === true || dream === true ? <h3 className="text-center text-5xl font-extrabold my-3" onClick={()=>{setShow(false)}}>Limpar</h3>:' '
       }
+        
+      
 
-    </section> 
+
+      {show && (gas && <ClickGas />)}
+
+      {show && (dream && <MyDream />)}
+
+
+
+    </section>
   );
 }
 
